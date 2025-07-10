f7f5891481a5926442f383a17c42e51bd24d7e81
Sahidvv <sahidvilic.wh@gmail.com>
2025-07-10 02:26:09 -0500

    first commit

--- src/notes/note.entity.ts
+++ src/notes/note.entity.ts
@@ -3,17 +3,15 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class Note {
   @PrimaryGeneratedColumn()
-  id: number;
-
+  id: number = 0;
   @Column()
-  title: string;
+  title: string = '';
 
   @Column({ default: '' })
-  content: string;
+  content: string = '';
 
   @Column({ default: false })
-  archived: boolean;
-
-  @Column('text', { array: true, nullable: true })
-  categories?: string[];
+  archived: boolean = false;
+  @Column('text', { array: true, nullable: true, default: [] })
+  categories?: string[] = [];
 }
