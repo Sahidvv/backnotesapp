2c7f37613af9499fff0b79e313c6da969c8e03bd
Sahidvv <sahidvilic.wh@gmail.com>
2025-07-10 02:10:50 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express'; // Importar el adaptador de Express
+import express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server)); // Usar Express como adaptador para NestJS
+
+  // Configuración de CORS
   app.enableCors({
     origin: [
       'https://frontnotes.vercel.app',
@@ -11,6 +16,7 @@ async function bootstrap() {
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
