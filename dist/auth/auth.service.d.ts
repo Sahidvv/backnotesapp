deb68febd7edbf5fe5e26412ea6bde065cc73071
Sahidvv <sahidvilic.wh@gmail.com>
2025-07-10 01:36:34 -0500

    Delete dist directory

--- dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class AuthService {
-    private readonly USER;
-    private readonly PASSWORD;
-    login(username: string, password: string): {
-        token: string;
-    };
-}
--- dist/notes/note.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Note {
-    id: number;
-    title: string;
-    content: string;
-    archived: boolean;
-    categories?: string[];
-}
