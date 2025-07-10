2f104eb0a9a89d35591807f39bfa6074fabab60c
Sahidvv <sahidvilic.wh@gmail.com>
2025-07-10 02:24:33 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express'; // Para usar Express en lugar del servidor predeterminado
-import * as express from 'express';
-
-const server = express(); // Crear la instancia de Express
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://frontnotes.vercel.app', 'https://backnotesapp.vercel.app'],
+    origin: [
+      'https://frontnotes.vercel.app',
+      'https://backnotesapp.vercel.app',
+    ],
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
-  await app.init();  // Asegurarse de que la app se inicializa correctamente
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
-bootstrap();
-
-export default server;  // Exportamos el servidor para que sea usado como función en Vercel
+void bootstrap();
