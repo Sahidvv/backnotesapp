c655f33e39cfa834e791376be636d1a59df8efdb
Sahidvv <sahidvilic.wh@gmail.com>
2025-07-10 01:57:17 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://frontnotes.vercel.app',
+      'https://backnotesapp.vercel.app',
+    ], // Permite los dominios
+    methods: 'GET,POST,PUT,DELETE,PATCH', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
